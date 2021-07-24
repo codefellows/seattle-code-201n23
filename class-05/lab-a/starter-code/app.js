3b74103bcb787f73db8c06e4b1651084f30410e1
Aaron Burke <aaron.burke.0104@gmail.com>
2021-07-24 13:31:24 -0700

    added sumArray functions

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -16,7 +16,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// sum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -37,7 +37,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// multiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,17 +54,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let sum = sum(a, b);
-  let sum = sum(sum, c);
+  let total = sum(a, b);
+  total = sum(total[0], c);
   let product = multiply(a, b);
-  let product = multiply(product, c)
-  let sumMessage = `${a} and ${b} and ${c} sum to ${sum}.`
+  product = multiply(product[0], c)
+  let sumMessage = `${a} and ${b} and ${c} sum to ${total}.`
   let productMessage = `The product of ${a} and ${b} and ${c} is ${product}.`
+  console.log(sum, product, sumMessage, productMessage);
   return [sum, product, sumMessage, productMessage];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// sumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -82,12 +83,22 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let total = 0
+  for (let i =0; i < sumArr.length; i++) {
+    if  (total === 0)  {
+      total = sum(sumArr[i], total); 
+    } else {
+      total = sum(sumArr[i], total[0]);
+    }
+  }
+  let message = `${testArray.join()} was passed in as an array of numbers, and ${total} is their sum.`
+  console.log(total, message);
+  return [total, message]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
