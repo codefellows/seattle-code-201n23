cad7302e5574a87945d3af9f37930ced26cfc53e
CHRISTINE LORA <CHRISTINELORA23@GMAIL.COM>
2021-07-24 16:41:23 -0700

    Problem 3 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -5,9 +5,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 "The sum of 4 and 7 is 11."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
+// // Write your code here
 function sum(a, b) { //eslint-disable-line
   let sums = a + b;
   let message = `The sum of ${a} and ${b} is ${sums}.`;
@@ -16,7 +16,7 @@ function sum(a, b) { //eslint-disable-line
   
 }
 
-// Here is the test for sum(); uncomment it to run it
+// // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -55,11 +55,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let summed = sum(a,b);
+  let finalsum = sum(summed[0], c);
 
+  let multiplied = multiply(a,b);
+  let multipliedfinal = multiply(multiplied[0], c);
+
+  let message = `${a} and ${b} and ${c} sum to ${finalsum[0]}.`;
+  let message2 = `The product of ${a} and ${b} and ${c} is ${multipliedfinal[0]}.`;
+  return [finalsum[0], multipliedfinal[0], message, message2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
