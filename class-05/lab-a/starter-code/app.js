0b6b1d6a63c676d91626aff6d6e44e1e5383451d
Shania Kimbrough <shania_nicolebb@yahoo.com>
2021-07-24 13:23:17 -0700

    problem 2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let multiply1 = a * b;
+  let string1 = `The product of ${a} and ${b} is ${multiply1}.`
+  return [multiply1, string1];
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
