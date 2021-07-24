ef188866c2e6a0175709848126ba8e08b59a8554
Chris Reichert <chris.a.reichert@gmail.com>
2021-07-24 14:56:01 -0500

    instructor completed problem01

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -15,6 +15,7 @@ function sum(a, b) { //eslint-disable-line
   return [sums, message];
 }
 
+// instructor completed problem 01
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -30,11 +31,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let product = a * b;
+  let message = `The product of ${a} and ${b} is ${product}.`;
+  console.log(product,message);
+  return [product,message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
