8b04423cb66166f1f3a242fc5d23489d05bfef33
Carrie Vo <carrienhivo@gmail.com>
2021-07-24 16:38:35 -0700

    modified problem 3 and adadded to problem3 branch.

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   let sums = a + b;
   let message = `The sum of ${a} and ${b} is ${sums}.`;
-  console.log(sums, message);
+  // console.log(sums, message);
   return [sums, message];
 }
 
@@ -32,7 +32,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   let multiplies = a * b;
   let message = `The product of ${a} and ${b} is ${multiplies}.`;
-  console.log(multiplies, message);
+  // console.log(multiplies, message);
   return [multiplies, message];
 }
 
@@ -55,12 +55,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let summed = sum(a, b);
+  let finalsum = sum(summed[0], c);
+
+  let multiplied = multiply(a, b);
+  let multipliedfinal = multiply(multiplied[0], c);
+
+  let message1 = `${a} and ${b} and ${c} sum to ${finalsum[0]}.`;
+  let message2 = `The product of ${a} and ${b} and ${c} is ${multipliedfinal[0]}.`;
+
+  return [finalsum[0], multipliedfinal[0], message1, message2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
