7a4326587da2ecdff2ef6b0e230f03c91964d3f7
Chris Rarig <cwrarig20@gmail.com>
2021-07-24 16:59:45 -0500

    updated problem02

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,16 +8,16 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { 
-//     let sums = (a + b);
-//     let message = `The sum of ${a} and ${b} is ${sums}.`;
-//     console.log(sums,message);
-//     return [sums,message];
+function sum(a, b) { 
+    let sums = (a + b);
+    let message = `The sum of ${a} and ${b} is ${sums}.`;
+    console.log(sums,message);
+    return [sums,message];
     
-// }
-// sum(4,7);
-// // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+}
+sum(4,7);
+// Here is the test for sum(); uncomment it to run it
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
