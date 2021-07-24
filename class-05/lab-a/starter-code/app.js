32d8145d660824c5cf2534e52004353ba3885014
CHRISTINE LORA <CHRISTINELORA23@GMAIL.COM>
2021-07-24 14:13:27 -0700

    Problem two solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let sums = a * b;
+  let message = `The product of ${a} and ${b} is ${sums}.`;
+  console.log(sums, message);
+  return [sums, message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
