19167386e05e60430d922fa2a3ad2c645511997d
Chris Reichert <chris.a.reichert@gmail.com>
2021-07-24 16:03:04 -0500

    problem 3 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -11,12 +11,12 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   let sums = a + b;
   let message = `The sum of ${a} and ${b} is ${sums}.`;
-  console.log(sums, message);
+  // console.log(sums, message);
   return [sums, message];
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,7 +30,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let product = a * b;
+  let message = `The product of ${a} and ${b} is ${product}.`;
+  // console.log(product, message);
+  return [product,message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -51,11 +54,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sumAB = sum(sum(a,b)[0], c)[0];
+  let productAB = multiply(multiply(a,b)[0],c)[0];
+  let messageA = `${a} and ${b} and ${c} sum to ${sumAB}.`;
+  let messageB = `The product of ${a} and ${b} and ${c} is ${productAB}.`;
+  // console.log('array: ',sumAB,productAB,messageA,messageB);
+  return [sumAB,productAB,messageA,messageB];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
