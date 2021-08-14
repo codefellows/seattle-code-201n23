f56c5cf13560be61784b29931e2ae7d9ce735e0a
sheyna <sheynawatkins@gmail.com>
2021-08-14 03:58:40 -0700

    updated files in class 11.

--- class-11/demo/audio-video/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const levees = document.getElementById('levees');
-const randomizer = document.getElementById('randomizer');
-
-randomizer.addEventListener('click', function(){
-  levees.volume = Math.random();
-  levees.autoplay = true;
-});
