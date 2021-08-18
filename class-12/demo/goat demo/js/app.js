fbcec7dd0055cb6352c005e053d0b35d4c5b0cd1
sheyna <sheynawatkins@gmail.com>
2021-08-17 20:34:32 -0700

    added demo code for goat project to class 12.

--- class-12/demo/goat demo/js/app.js
+++ class-12/demo/goat demo/js/app.js
@@ -4,11 +4,12 @@
 let allGoats = [];
 let clicks = 0;
 let clicksAllowed = 15;
+let numberOfUniqueIndexes = 4;
 
 let myContainer = document.querySelector('section');
-let myButton = document.querySelector('section + div');
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
+let indexArray = [];
 
 function Goat(name, fileExtension = 'jpg') {
   this.name = name;
@@ -33,15 +34,28 @@ function selectRandGoat() {
   return Math.floor(Math.random() * allGoats.length);
 }
 
-function renderGoats()  {
-  let goat1 = selectRandGoat();
-  let goat2 = selectRandGoat();
-  // seriously consider using an array.
-  // remember: how do you know if an array includes something?
-  // Google it and find out.
-  while (goat1 === goat2) {
-    goat2 = selectRandGoat();
+// .pop() - removes last item
+// .push() - append to the end of an array
+// .shift() - removes the first item
+// .unshift() - add to the beginning of an array
+
+function renderGoats() {
+  while (indexArray.length < numberOfUniqueIndexes) {
+    let randomNumber = selectRandGoat();
+    if (!indexArray.includes(randomNumber)) {
+      indexArray.push(randomNumber);
+    }
   }
+  // console.log(indexArray);
+  let goat1 = indexArray.shift();
+  let goat2 = indexArray.shift();
+  // console.log(indexArray);
+  // // seriously consider using an array.
+  // // remember: how do you know if an array includes something?
+  // // Google it and find out.
+  // while (goat1 === goat2) {
+  //   goat2 = selectRandGoat();
+  // }
   image1.src = allGoats[goat1].src;
   image1.alt = allGoats[goat1].name;
   allGoats[goat1].views++;
@@ -64,27 +78,63 @@ function handleGoatClick(event) {
   }
   renderGoats();
   if (clicks === clicksAllowed) {
-    myButton.className = 'clicks-allowed';
     myContainer.removeEventListener('click', handleGoatClick);
+    renderChart();
   }
 }
 
-function handleButtonClick(event) {
-  if (clicks === clicksAllowed) {
-    renderResults();
-  }
-}
+// function handleButtonClick(event) {
+//   if (clicks === clicksAllowed) {
+//     renderResults();
+//   }
+// }
 
-function renderResults() {
-  let ul = document.querySelector('ul');
-  for (let i = 0; i < allGoats.length; i++) {
-    let li = document.createElement('li')
-    li.textContent = `${allGoats[i].name} had ${allGoats[i].views} view and was clicked ${allGoats[i].clicks} times.`;
-    ul.appendChild(li);
-  }
-}
+
 
 renderGoats();
 
+
+function renderChart() {
+  let goatClicks = [];
+  let goatViews = [];
+  let goatNames = [];
+  for (let i = 0; i < allGoats.length; i++) {
+    goatNames.push(allGoats[i].name);
+    goatClicks.push(allGoats[i].clicks);
+    goatViews.push(allGoats[i].views);
+  }
+  //console.log(goatClicks);
+  let chartObject = {
+    type: 'bar',
+    data: {
+      labels: goatNames,
+      datasets: [{
+        label: '# of Views',
+        data: goatViews,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      },
+      {
+        label: '# of Clicks',
+        data: goatClicks,
+        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        borderColor: 'rgba(255, 206, 86, 1)',
+        borderWidth: 1
+      }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, chartObject);
+}
+
+
 myContainer.addEventListener('click', handleGoatClick);
-myButton.addEventListener('click', handleButtonClick);
